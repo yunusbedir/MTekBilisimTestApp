d218408d3cf1902b891456b77d93c5a1976d53f4
yunus bedir <yunusbedir@outlook.com.tr>
2020-03-19 15:39:42 +0300

    resim ve description kısımlarındaki ufak hatalar düzeltildi

--- app/src/main/java/com/yunusbedir/mtekbilisimtestapp/adapter/newsAdapter/NewsViewHolder.kt
+++ app/src/main/java/com/yunusbedir/mtekbilisimtestapp/adapter/newsAdapter/NewsViewHolder.kt
@@ -2,6 +2,7 @@ package com.yunusbedir.mtekbilisimtestapp.adapter.newsAdapter
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.yunusbedir.mtekbilisimtestapp.model.Items
 import kotlinx.android.synthetic.main.item_news.view.*
 
@@ -13,9 +14,9 @@ class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     fun bind(newsModel: Items, onClick: (newModel: Items) -> Unit) {
         itemView.tvTitle.text = newsModel.title
-        itemView.tvDescription.text = newsModel.description
+        itemView.tvDescription.text = newsModel.description.split(">")[1]
         itemView.tvDate.text = newsModel.pubDate
-        //set Image glide
+        Glide.with(itemView).load(newsModel.thumbnail).centerCrop().into(itemView.imgNews)
 
         itemView.setOnClickListener {
             onClick(newsModel)
